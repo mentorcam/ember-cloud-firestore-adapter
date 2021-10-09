3ac98e366d5acd42176de6492a13bca084b411d0
Benjamin Balazs <benjaminbalazs@gmail.com>
2021-10-09 15:49:51 +0200

    fix

--- index.js
+++ index.js
@@ -19,12 +19,12 @@ module.exports = {
   included(app) {
     this._super.included.apply(this, arguments);
 
-    app.import('vendor/fastboot-shims/firebase/firebase-firestore.js');
-    app.import('vendor/fastboot/firebase-firestore.js');
+    //app.import('vendor/fastboot-shims/firebase/firebase-firestore.js');
+    //app.import('vendor/fastboot/firebase-firestore.js');
 
     if (app.env !== 'production') {
-      app.import('node_modules/mock-cloud-firestore/dist/mock-cloud-firestore.js');
-      app.import('vendor/shims/mock-cloud-firestore.js');
+      //app.import('node_modules/mock-cloud-firestore/dist/mock-cloud-firestore.js');
+      //app.import('vendor/shims/mock-cloud-firestore.js');
     }
   },
 };
